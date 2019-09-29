cfa239dcc4b4d3b360b2ecea9508f940392f5adb
zhonghui <zhonghui@songguo7.com>
2019-09-29 13:40:53 +0800

    解决库加载偶尔超时问题

--- dist/libs/baidumap.js
+++ dist/libs/baidumap.js
@@ -4,40 +4,56 @@ System.register([], function (_export, _context) {
     "use strict";
 
     function loadJsFile(fileName) {
+        var reject = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
         var fileElement = document.createElement('script');
         fileElement.setAttribute('type', 'text/javascript');
         fileElement.setAttribute('src', fileName);
+        if (reject) {
+            fileElement.onerror = reject;
+        }
         document.head.appendChild(fileElement);
     }
 
     function loadCssFile(fileName) {
+        var reject = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
         var fileElement = document.createElement('link');
         fileElement.setAttribute('rel', 'stylesheet');
         fileElement.setAttribute('type', 'text/css');
         fileElement.setAttribute('href', fileName);
+        if (reject) {
+            fileElement.onerror = reject;
+        }
         document.head.appendChild(fileElement);
+    }
+
+    function waitLoading(checkValue, fun, checkTime, maxTime) {
+        if (checkValue && maxTime > checkTime) {
+            fun();
+        } else {
+            setTimeout(function () {
+                return waitLoading(checkValue, fun, checkTime, maxTime - checkTime);
+            }, checkTime);
+        }
     }
 
     function MP(ak) {
         return new Promise(function (resolve, reject) {
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'http://api.map.baidu.com/api?v=3.0&ak=' + ak + '&callback=init';
-            script.onerror = reject;
-            document.head.appendChild(script);
+            loadJsFile('http://api.map.baidu.com/api?v=3.0&ak=' + ak + '&callback=init', reject);
+            waitLoading(BMap, function () {
+                loadJsFile('http://api.map.baidu.com/library/TextIconOverlay/1.2/src/TextIconOverlay_min.js', reject);
+                loadJsFile('http://api.map.baidu.com/library/MarkerClusterer/1.2/src/MarkerClusterer_min.js', reject);
+                loadJsFile('http://api.map.baidu.com/library/Heatmap/2.0/src/Heatmap_min.js', reject);
+                loadJsFile('http://api.map.baidu.com/library/DistanceTool/1.2/src/DistanceTool_min.js', reject);
+                loadJsFile('http://api.map.baidu.com/library/RectangleZoom/1.2/src/RectangleZoom_min.js', reject);
 
-            setTimeout(function () {
-                loadJsFile('http://api.map.baidu.com/library/TextIconOverlay/1.2/src/TextIconOverlay_min.js');
-                loadJsFile('http://api.map.baidu.com/library/MarkerClusterer/1.2/src/MarkerClusterer_min.js');
-                loadJsFile('http://api.map.baidu.com/library/Heatmap/2.0/src/Heatmap_min.js');
-                loadJsFile('http://api.map.baidu.com/library/DistanceTool/1.2/src/DistanceTool_min.js');
-                loadJsFile('http://api.map.baidu.com/library/RectangleZoom/1.2/src/RectangleZoom_min.js');
-
-                loadJsFile('http://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.js');
-                loadCssFile('http://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.css');
-
-                resolve(BMap);
-            }, 500);
+                loadJsFile('http://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.js', reject);
+                loadCssFile('http://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.css', reject);
+                waitLoading(BMapLib, function () {
+                    resolve(BMap);
+                }, 100, 60000);
+            }, 100, 60000);
         });
     }
 
--- src/libs/baidumap.js
+++ src/libs/baidumap.js
@@ -1,37 +1,47 @@
-function loadJsFile(fileName) {
+function loadJsFile(fileName, reject = null) {
     const fileElement = document.createElement('script');
     fileElement.setAttribute('type', 'text/javascript');
     fileElement.setAttribute('src', fileName);
+    if (reject) {
+        fileElement.onerror = reject;
+    }
     document.head.appendChild(fileElement);
 }
 
-function loadCssFile(fileName) {
+function loadCssFile(fileName, reject = null) {
     const fileElement = document.createElement('link');
     fileElement.setAttribute('rel', 'stylesheet');
     fileElement.setAttribute('type', 'text/css');
     fileElement.setAttribute('href', fileName);
+    if (reject) {
+        fileElement.onerror = reject;
+    }
     document.head.appendChild(fileElement);
+}
+
+function waitLoading(checkValue, fun, checkTime, maxTime) {
+    if (checkValue && maxTime > checkTime) {
+        fun();
+    } else {
+        setTimeout(() => waitLoading(checkValue, fun, checkTime, maxTime - checkTime), checkTime);
+    }
 }
 
 export function MP(ak) {
     return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'http://api.map.baidu.com/api?v=3.0&ak=' + ak + '&callback=init';
-        script.onerror = reject;
-        document.head.appendChild(script);
+        loadJsFile('http://api.map.baidu.com/api?v=3.0&ak=' + ak + '&callback=init', reject);
+        waitLoading(BMap, () => {
+            loadJsFile('http://api.map.baidu.com/library/TextIconOverlay/1.2/src/TextIconOverlay_min.js', reject);
+            loadJsFile('http://api.map.baidu.com/library/MarkerClusterer/1.2/src/MarkerClusterer_min.js', reject);
+            loadJsFile('http://api.map.baidu.com/library/Heatmap/2.0/src/Heatmap_min.js', reject);
+            loadJsFile('http://api.map.baidu.com/library/DistanceTool/1.2/src/DistanceTool_min.js', reject);
+            loadJsFile('http://api.map.baidu.com/library/RectangleZoom/1.2/src/RectangleZoom_min.js', reject);
 
-        setTimeout(() => {
-            loadJsFile('http://api.map.baidu.com/library/TextIconOverlay/1.2/src/TextIconOverlay_min.js');
-            loadJsFile('http://api.map.baidu.com/library/MarkerClusterer/1.2/src/MarkerClusterer_min.js');
-            loadJsFile('http://api.map.baidu.com/library/Heatmap/2.0/src/Heatmap_min.js');
-            loadJsFile('http://api.map.baidu.com/library/DistanceTool/1.2/src/DistanceTool_min.js');
-            loadJsFile('http://api.map.baidu.com/library/RectangleZoom/1.2/src/RectangleZoom_min.js');
-
-            loadJsFile('http://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.js');
-            loadCssFile('http://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.css');
-
-            resolve(BMap);
-        }, 500);
+            loadJsFile('http://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.js', reject);
+            loadCssFile('http://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.css', reject);
+            waitLoading(BMapLib, () => {
+                resolve(BMap);
+            }, 100, 60000);
+        }, 100, 60000);
     });
 }
