3dd1d85f2049761272751e69af2c1df2f834e997
zhonghui <zhonghui@songguo7.com>
2019-10-23 20:43:48 +0800

    update

--- dist/map_renderer.js
+++ dist/map_renderer.js
@@ -28,79 +28,80 @@ System.register(['./css/leaflet.css!', './libs/baidumap.js'], function (_export,
                         enableMapClick: ctrl.panel.enableMapClick
                     });
                     ctrl.map.centerAndZoom(new BMap.Point(ctrl.panel.lng, ctrl.panel.lat), parseInt(ctrl.panel.initialZoom, 10));
-                    ctrl.map.enableScrollWheelZoom();
-                    ctrl.map.setMapStyle({ style: ctrl.panel.theme });
+                    // ctrl.map.enableScrollWheelZoom();
+                    // ctrl.map.setMapStyle({style: ctrl.panel.theme});
 
-                    ctrl.navigationSwitch = new BMap.NavigationControl();
-                    ctrl.scaleSwitch = new BMap.ScaleControl();
+                    // ctrl.navigationSwitch = new BMap.NavigationControl();
+                    // ctrl.scaleSwitch = new BMap.ScaleControl();
                     // ctrl.overviewMapSwitch = new BMap.OverviewMapControl({
                     //     isOpen: true,
                     //     anchor: BMAP_ANCHOR_BOTTOM_RIGHT
                     // });
                     // ctrl.mapTypeSwitch = new BMap.MapTypeControl();
 
-                    if (ctrl.panel.navigation === true) ctrl.map.addControl(ctrl.navigationSwitch);
-                    if (ctrl.panel.scale === true) ctrl.map.addControl(ctrl.scaleSwitch);
+                    // if (ctrl.panel.navigation === true) ctrl.map.addControl(ctrl.navigationSwitch);
+                    // if (ctrl.panel.scale === true) ctrl.map.addControl(ctrl.scaleSwitch);
                     // if (ctrl.panel.overviewMap === true) ctrl.map.addControl(ctrl.overviewMapSwitch);
                     // if (ctrl.panel.mapType === true) ctrl.map.addControl(ctrl.mapTypeSwitch);
 
-                    ctrl.map.addEventListener('dragend', function () {
-                        var center = ctrl.map.getCenter();
-                        ctrl.panel.lat = center.lat;
-                        ctrl.panel.lng = center.lng;
-                    });
-
-                    ctrl.map.addEventListener('click', function (event) {
-                        if (ctrl.clickHandler && ctrl.clickHandler.length > 0) {
-                            ctrl.clickHandler.forEach(function (handler) {
-                                return handler(event);
-                            });
-                        }
-                    }, true);
+                    // ctrl.map.addEventListener('dragend', function () {
+                    //     const center = ctrl.map.getCenter();
+                    //     ctrl.panel.lat = center.lat;
+                    //     ctrl.panel.lng = center.lng;
+                    // });
+                    //
+                    // ctrl.map.addEventListener('click', function (event) {
+                    //     if (ctrl.clickHandler && ctrl.clickHandler.length > 0) {
+                    //         ctrl.clickHandler.forEach(handler => handler(event));
+                    //     }
+                    // }, true);
 
                     // eslint-disable-next-line no-unused-expressions
-                    ctrl.distanceTool = new BMapLib.DistanceTool(ctrl.map);
-                    ctrl.rectangleZoomTool = new BMapLib.RectangleZoom(ctrl.map, {
-                        followText: '拖拽鼠标进行操作',
-                        autoClose: true
-                    });
+                    // ctrl.distanceTool = new BMapLib.DistanceTool(ctrl.map);
+                    // ctrl.rectangleZoomTool = new BMapLib.RectangleZoom(ctrl.map, {
+                    //     followText: '拖拽鼠标进行操作',
+                    //     autoClose: true
+                    // });
+                    //
+                    // ctrl.trafficSwitch = new BMapLib.TrafficControl({
+                    //     showPanel: false, // 是否显示路况提示面板
+                    // });
+                    // ctrl.trafficSwitch.setOffset(new BMap.Size(10, 40));
+                    // // ctrl.trafficSwitch.setAnchor(BMAP_ANCHOR_BOTTOM_RIGHT);
+                    // if (ctrl.panel.traffic === true) {
+                    //     ctrl.map.addControl(ctrl.trafficSwitch);
+                    // }
 
-                    ctrl.trafficSwitch = new BMapLib.TrafficControl({
-                        showPanel: false // 是否显示路况提示面板
-                    });
-                    ctrl.trafficSwitch.setOffset(new BMap.Size(10, 40));
-                    // ctrl.trafficSwitch.setAnchor(BMAP_ANCHOR_BOTTOM_RIGHT);
-                    if (ctrl.panel.traffic === true) {
-                        ctrl.map.addControl(ctrl.trafficSwitch);
-                    }
-
-                    var menu = new BMap.ContextMenu();
-                    var txtMenuItem = [{
-                        text: '测距',
-                        callback: function callback() {
-                            ctrl.distanceTool.open();
-                        }
-                    }, {
-                        text: '拉框放大',
-                        callback: function callback() {
-                            ctrl.rectangleZoomTool.open();
-                        }
-                    }, {
-                        text: '移到中心点',
-                        callback: function callback() {
-                            ctrl.panToCenterPoint();
-                        }
-                    }, {
-                        text: '重置地图',
-                        callback: function callback() {
-                            ctrl.map.reset();
-                        }
-                    }];
-
-                    for (var menuIndex = 0; menuIndex < txtMenuItem.length; menuIndex++) {
-                        menu.addItem(new BMap.MenuItem(txtMenuItem[menuIndex].text, txtMenuItem[menuIndex].callback, 100));
-                    }
-                    ctrl.map.addContextMenu(menu);
+                    // const menu = new BMap.ContextMenu();
+                    // const txtMenuItem = [
+                    //     {
+                    //         text: '测距',
+                    //         callback: function () {
+                    //             ctrl.distanceTool.open();
+                    //         }
+                    //     },
+                    //     {
+                    //         text: '拉框放大',
+                    //         callback: function () {
+                    //             ctrl.rectangleZoomTool.open();
+                    //         }
+                    //     }, {
+                    //         text: '移到中心点',
+                    //         callback: function callback() {
+                    //             ctrl.panToCenterPoint();
+                    //         }
+                    //     }, {
+                    //         text: '重置地图',
+                    //         callback: function callback() {
+                    //             ctrl.map.reset();
+                    //         }
+                    //     },
+                    // ];
+                    //
+                    // for (let menuIndex = 0; menuIndex < txtMenuItem.length; menuIndex++) {
+                    //     menu.addItem(new BMap.MenuItem(txtMenuItem[menuIndex].text, txtMenuItem[menuIndex].callback, 100));
+                    // }
+                    // ctrl.map.addContextMenu(menu);
                     // ctrl.addNode(BMap);
                 });
             }
