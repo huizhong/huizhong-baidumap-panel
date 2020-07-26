aee2e04f681db0457f86127da193d4b553fddc83
auto.builder <noreply@auto.builder>
2020-07-26 04:48:40 +0000

    auto.builder

--- dist/libs/baidumap.js
+++ dist/libs/baidumap.js
@@ -41,18 +41,18 @@ System.register([], function (_export, _context) {
 
     function MP(ak) {
         return new Promise(function (resolve, reject) {
-            loadJsFile('http://api.map.baidu.com/api?v=3.0&ak=' + ak + '&callback=init', reject);
+            loadJsFile('https://api.map.baidu.com/api?v=3.0&ak=' + ak + '&callback=init', reject);
             waitLoading(function () {
                 return typeof BMap !== 'undefined' && typeof BMap.Map !== 'undefined';
             }, function () {
-                loadJsFile('http://api.map.baidu.com/library/TextIconOverlay/1.2/src/TextIconOverlay_min.js', reject);
-                loadJsFile('http://api.map.baidu.com/library/MarkerClusterer/1.2/src/MarkerClusterer_min.js', reject);
-                loadJsFile('http://api.map.baidu.com/library/Heatmap/2.0/src/Heatmap_min.js', reject);
-                loadJsFile('http://api.map.baidu.com/library/DistanceTool/1.2/src/DistanceTool_min.js', reject);
-                loadJsFile('http://api.map.baidu.com/library/RectangleZoom/1.2/src/RectangleZoom_min.js', reject);
+                loadJsFile('https://api.map.baidu.com/library/TextIconOverlay/1.2/src/TextIconOverlay_min.js', reject);
+                loadJsFile('https://api.map.baidu.com/library/MarkerClusterer/1.2/src/MarkerClusterer_min.js', reject);
+                loadJsFile('https://api.map.baidu.com/library/Heatmap/2.0/src/Heatmap_min.js', reject);
+                loadJsFile('https://api.map.baidu.com/library/DistanceTool/1.2/src/DistanceTool_min.js', reject);
+                loadJsFile('https://api.map.baidu.com/library/RectangleZoom/1.2/src/RectangleZoom_min.js', reject);
 
-                loadJsFile('http://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.js', reject);
-                loadCssFile('http://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.css', reject);
+                loadJsFile('https://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.js', reject);
+                loadCssFile('https://api.map.baidu.com/library/TrafficControl/1.4/src/TrafficControl_min.css', reject);
                 waitLoading(function () {
                     return typeof BMapLib !== 'undefined' && typeof BMapLib.HeatmapOverlay !== 'undefined' && typeof BMapLib.MarkerClusterer !== 'undefined' && typeof BMapLib.DistanceTool !== 'undefined' && typeof BMapLib.RectangleZoom !== 'undefined' && typeof BMapLib.TrafficControl !== 'undefined';
                 }, function () {
